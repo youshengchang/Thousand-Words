b02239fc5c0490392982db32be1d1a4aaf1af9b0
youshengchang <changyousheng@yahoo.com>
2014-08-18 19:56:05 -0700

    Initial Commit

---
+++ Thousand Words/TWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TWAppDelegate.h
+//  Thousand Words
+//
+//  Created by yousheng chang on 8/18/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by yousheng chang on 8/18/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
